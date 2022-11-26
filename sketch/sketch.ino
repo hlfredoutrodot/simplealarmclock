25d3684294ddec6e5e29fdabd9987ab519d3cbd2
Stbretzel <helie.soleil@gmail.com>
2022-11-26 15:12:40 +0100

    Start adding buttons

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -19,6 +19,7 @@ int minutespeed = 60000;
 bool debugmode = false;
 String lang = "fr";
 
+int milliscount;
 String day1 = "day1";
 String day2 = "day2";
 String day3 = "day3";
@@ -27,11 +28,12 @@ String day5 = "day5";
 String day6 = "day6";
 String day7 = "day7";
 String trweek = "defweek";
-String startscrl1 = "blank line 1";
-String startscrl2 = "blank line 2";
+String startscrl1 = "SIMPLEalarmCLOCK";
+String startscrl2 = "   @Stbretzel   ";
 
 
 int config = 1;
+
 
 void setup() {
 
@@ -46,6 +48,9 @@ void setup() {
   }
 
   pinMode(A4, OUTPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
   digitalWrite(A4, HIGH);
   Serial.begin(9600);
   lcd.begin(16, 2);
@@ -65,8 +70,6 @@ void setup() {
     String startscrl2 = "use as an alarmclock";
   }
 
-  //while(config < 10){}
-
   if (lang == "fr") {
     day1 = "Lundi";
     day2 = "Mardi";
@@ -78,96 +81,131 @@ void setup() {
     trweek = "semaine: ";
     if (debugmode == true) {
       Serial.println("lang = fr");
+      lcd.clear();
+      lcd.setCursor(11, 0);
+      lcd.print(":");
+      if (hour < 10) {
+        lcd.setCursor(9, 0);
+        lcd.print(week);
+
+        lcd.print("0");
+        lcd.setCursor(10, 0);
+        lcd.print(hour);
+       lcd.print(trweek);
+        lcd.print(week);
+      }
+    }
+
+    if (lang == "en") {
+      day1 = "Monday";
+      day2 = "Tuesday";
+      day3 = "Wednesday";
+      day4 = "Thursday";
+      day5 = "Friday";
+      day6 = "Saturday";
+      day7 = "Sunday";
+      trweek = "  week : ";
+      if (debugmode == true) {
+        Serial.println("lang = en");
+      }
     }
   }
+}  
 
-  if (lang == "en") {
-    day1 = "Monday";
-    day2 = "Tuesday";
-    day3 = "Wednesday";
-    day4 = "Thursday";
-    day5 = "Friday";
-    day6 = "Saturday";
-    day7 = "Sunday";
-    trweek = "  week : ";
-    if (debugmode == true) {
-      Serial.println("lang = en");
-    }
-  }
-}
+void hourpass() {
+      minute = minute + 1;
+    if (minute == 60) {
+      hour = hour + 1;
+      minute = 00;
 
-void loop() {
+      if (hour == 24) {
+        day = day + 1;
+        hour = 00;
 
-  minute = minute + 1;
-  if (minute == 60) {
-    hour = hour + 1;
-    minute = 00;
-
-    if (hour == 24) {
-      day = day + 1;
-      hour = 00;
-
-      if (day == 8) {
-        if (week == 1) {
-          week = 2;
-          day = 1;
-        }
-        if (week == 2) {
-          week = 1;
-          day = 1;
+        if (day == 8) {
+          if (week == 1) {
+            week = 2;
+            day = 1;
+          }
+          if (week == 2) {
+            week = 1;
+            day = 1;
+          }
         }
       }
     }
   }
 
-  lcd.clear();
-  lcd.setCursor(11, 0);
-  lcd.print(":");
-  if (hour < 10) {
-    lcd.setCursor(9, 0);
-    lcd.print("0");
-    lcd.setCursor(10, 0);  // put your main code here, to run repeatedly:
-    lcd.print(hour);
-  } else {
-    lcd.setCursor(9, 0);
-    lcd.print(hour);
-  }
-  if (minute < 10) {
-    lcd.setCursor(12, 0);
-    lcd.print("0");
-    lcd.setCursor(13, 0);
-    lcd.print(minute);
-  } else {
-    lcd.setCursor(12, 0);
-    lcd.print(minute);
-  }
-  lcd.setCursor(0, 0);
+  void hourshow() {
 
-  if (day == 1) {
-    lcd.print(day1);
+    lcd.clear();
+    lcd.setCursor(11, 0);
+    lcd.print(":");
+    if (hour < 10) {
+      lcd.setCursor(9, 0);
+      lcd.print(week);
+
+      lcd.print("0");
+      lcd.setCursor(10, 0); 
+      lcd.print(hour);
+    } else {
+      lcd.setCursor(9, 0);
+      lcd.print(week);
+
+      lcd.print(hour);
+    }
+    if (minute < 10) {
+      lcd.setCursor(12, 0);
+      lcd.print("0");
+      lcd.setCursor(13, 0);
+      lcd.print(minute);
+    } else {
+      lcd.setCursor(12, 0);
+      lcd.print(minute);
+    }
+    lcd.setCursor(0, 0);
+
+    if (day == 1) {
+      lcd.print(day1);
+    }
+    if (day == 2)   hourpass();
+  delay(minutespeed);{
+      lcd.print(day2);
+    }
+    if (day == 3) {
+      lcd.print(day3);
+    }
+    if (day == 4) {
+      lcd.print(day4);
+    }
+    if (day == 5) {
+      lcd.print(day5);
+    }
+    if (day == 6) {
+      lcd.print(day6);
+    }
+    if (day == 7) {
+      lcd.print(day7);
+    }
+
+    lcd.setCursor(0, 1);
+    lcd.print(trweek);
+    lcd.print(week);
   }
-  if (day == 2) {
-    lcd.print(day2);
-  }
-  if (day == 3) {
-    lcd.print(day3);
-  }
-  if (day == 4) {
-    lcd.print(day4);
-  }
-  if (day == 5) {
-    lcd.print(day5);
-  }
-  if (day == 6) {
-    lcd.print(day6);
-  }
-  if (day == 7) {
-    lcd.print(day7);
+  
+  void loop() {void hourpass() {
+      minute = minute + 1;
+    if (minute == 60) {
+      hour = hour + 1;
+…    }
+
+    lcd.setCursor(0, 1);
+    lcd.print(trweek);
+    lcd.print(week);
   }
 
-  lcd.setCursor(0, 1);
-  lcd.print(trweek);
-  lcd.print(week);
-
-  delay(minutespeed);
-}
+    milliscount = 0;
+    while (milliscount = minutespeed) {
+      milliscount++;
+    }
+  }
